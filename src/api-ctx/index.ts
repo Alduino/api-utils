bdd87fe8095cbee110cb081ac91c62f561075bc1
Alduino <git@alduino.dev>
2021-08-30 14:36:35 +1000

    Export ApiContext type

--- src/api-ctx/index.ts
+++ src/api-ctx/index.ts
@@ -1,4 +1,3 @@
 export type {default as ApiContextType} from "./ApiContextType";
 export {default as createContext} from "./create";
-export * from "./ApiContext";
-export type {default} from "./ApiContext";
+export type {default, ApiProviderProps} from "./ApiContext";
