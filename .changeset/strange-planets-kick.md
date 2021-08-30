8d57891ac6a5eef6d18afaf7226fd334ecde4481
Zach Barham <git@alduino.dev>
2021-08-30 14:39:37 +1000

    Export ApiContext type (#7)
    So that `createContext()` can be used with Typescript

---
+++ .changeset/strange-planets-kick.md
@@ -0,0 +1,5 @@
+---
+"@alduino/api-utils": patch
+---
+
+Export ApiContext type so that createContext() can be used with Typescript
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export {default as key} from "./key";
 export type {Param, ParamRecord} from "./key";
 export {createContext} from "./api-ctx";
-export type {ApiProviderProps} from "./api-ctx";
+export type {default as ApiContext, ApiProviderProps} from "./api-ctx";
 export * from "./swr";
 export type {SWRConfiguration, SWRResponse} from "swr";
 export type {SWRInfiniteConfiguration, SWRInfiniteResponse} from "swr/infinite";
